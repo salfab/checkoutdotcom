e09e134e325493b6d8f8504ffed6091c1e70e872
Fabio Salvalai <fabio@seesharp.ch>
2017-02-25 12:45:08 +0100

    prepare integration tests

---
+++ checkoutdotcom.Tests/IntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace checkoutdotcom.Tests
+{
+    using System.Net;
+
+    using FluentAssertions;
+
+    using RestSharp;
+
+    [TestClass]
+    public class IntegrationTests
+    {
+        private string serviceUrl;
+
+        [TestInitialize]
+        public void InitTestServer()
+        {
+            this.serviceUrl = "http://localhost:52157";
+            var client = new RestClient(this.serviceUrl);
+            client.Get(new RestRequest("/")).ResponseStatus.Should().NotBe(ResponseStatus.Error, $"The service should be running on url {this.serviceUrl}. Please run the web application before executing the integration tests.");
+
+            // TODO: Use kestrel here to start an instance instead of asking the app to be run manually.
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

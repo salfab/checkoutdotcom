6acdfe43673d7313e3e5c08d95bb09cfc16d845e
Fabio Salvalai <fabio@seesharp.ch>
2017-02-25 14:42:07 +0100

    fix service registration

--- src/checkoutdotcom/Startup.cs
+++ src/checkoutdotcom/Startup.cs
@@ -32,7 +32,7 @@ namespace checkoutdotcom
             // Add framework services.
             services.AddMvc();
 
-            services.AddTransient<IDrinksPersistenceService, DrinksPersistenceService>();
+            services.AddSingleton<IDrinksPersistenceService, DrinksPersistenceService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

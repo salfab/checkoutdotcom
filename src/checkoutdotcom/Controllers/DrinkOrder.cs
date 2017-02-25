6364a364bbc7ac36b0c3cc414d3fe6217be845ee
Fabio Salvalai <fabio@seesharp.ch>
2017-02-25 13:34:37 +0100

    implement DinkOrder

--- src/checkoutdotcom/Controllers/DrinkOrder.cs
+++ src/checkoutdotcom/Controllers/DrinkOrder.cs
@@ -2,5 +2,14 @@
 {
     public class DrinkOrder
     {
+        public DrinkOrder(string name, int quantity)
+        {
+            this.Name = name;
+            this.Quantity = quantity;
+        }
+
+        public string Name { get; private set; }
+
+        public int Quantity { get; private set; }
     }
 }
